693ce99af3b244f8d1928744ef48aa8aea4533d7
Leksusmart <leksusmartmail@gmail.com>
2024-08-26 15:11:12 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+// main.cpp
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("file:///A:/projects/cpp/Minesweeper/src/MainWindow.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
